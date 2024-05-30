b28be776af31e4c5d5f0c72f006c47c1caa0a760
catrt <rt990122@gmail.com>
2024-05-30 11:33:29 +0900

    Add: button component 추가

---
+++ src/app/correct/page.tsx
@@ -0,0 +1,13 @@
+import Button from '@/components/Button';
+import React from 'react';
+
+function CorrectPage() {
+  return (
+    <>
+      <Button>정정 신청</Button>
+      <Button buttonStyle="invert">정정 취소</Button>
+    </>
+  );
+}
+
+export default CorrectPage;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  buttonStyle?: 'default' | 'invert';
+}
+
+function Button({
+  buttonStyle = 'default',
+  type = 'button',
+  children,
+}: ButtonProps) {
+  let tailwind;
+  switch (buttonStyle) {
+    case 'invert':
+      tailwind =
+        'flex justify-center items-center bg-white font-bold text-violet-500 rounded-xl py-2.5 px-12 border border-violet-500';
+      break;
+    default:
+      tailwind =
+        'flex justify-center items-center bg-violet-500 font-bold text-white rounded-xl py-2.5 px-12';
+  }
+
+  return (
+    <button className={tailwind} type={type || 'button'}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  buttonStyle: 'default',
+};
+
+export default Button;
