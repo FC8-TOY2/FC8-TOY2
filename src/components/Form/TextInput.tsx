2837a403a9c0548eeea6b6ca4b9a8a0693c3c1f1
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-06-02 15:24:22 +0900

    Fix : Input css 수정

--- src/components/Form/TextInput.tsx
+++ src/components/Form/TextInput.tsx
@@ -5,6 +5,7 @@ interface TextInputProps {
   value: string;
   placeholder?: string;
   error?: string | null;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 function TextInput({
@@ -14,25 +15,22 @@ function TextInput({
   placeholder,
   value,
   error,
+  ...props
 }: TextInputProps) {
   return (
-    <label htmlFor={name} className="w-full flex flex-col gap-2 text-sm">
+    <label htmlFor={name} className="w-full flex flex-col gap-1 text-sm">
       {label}
       <input
         type={type}
         name={name}
         placeholder={placeholder}
-        className="border border-violet-400 text-base p-2 rounded-xl"
+        className="border border-violet-400 text-base p-2 rounded-xl w-full placeholder:font-light placeholder:text-sm"
         value={value}
+        {...props}
       />
-      {error && <span className="text-sm px-1 text-red-300">{error}</span>}
+      {error && <span className="text-xs px-1 text-red-300">{error}</span>}
     </label>
   );
 }
-
-TextInput.defaultProps = {
-  placeholder: '',
-  error: null,
-};
 
 export default TextInput;
