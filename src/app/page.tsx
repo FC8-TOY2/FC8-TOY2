beea01087bccdb2a9cbf10503515e372e1e4b60a
catrt <rt990122@gmail.com>
2024-06-09 00:27:06 +0900

    Simplify: main 페이지 layout 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Banner from '@/components/Banner';
+import Header from '@/components/Header';
+import SideBar from '@/components/SideBar';
 
 export default function Home() {
   return (
-    <main>
-      <Banner />
-    </main>
+    <>
+      <Header />
+      <SideBar />
+      <main className="pt-[130px] pr-[50px] pl-[310px] pb-[30px]">
+        <Banner />
+      </main>
+    </>
   );
 }
